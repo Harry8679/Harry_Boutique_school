e9217ed7833207e93245958f0189f5f3fde0d506
Harry EMARH <emarh.harry.code@gmail.com>
2024-02-21 09:01:08 +0100

    Setup of our application, Homepage created and User Entity

---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomePageController extends AbstractController
+{
+    #[Route('/', name: 'app_home_page')]
+    public function index(): Response
+    {
+        return $this->render('home_page/index.html.twig');
+    }
+}
